6011fe33c61f5f6ccb190af7d90cd335aa5ceb53
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-20 11:44:16 -0400

    Error page is now a functioning page with styled-componenet

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import img from '../assets/images/not-found.svg';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <Wrapper>
+      <div className="full-page">
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/"> Back Home </Link>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Error
+const Wrapper = styled.main`
+  text-align: center;
+
+  img {
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+    margin-top: 10rem;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: var(--grey-500);
+  }
+  a {
+    color: var(--primary-500);
+    text-decoration: underline;
+    text-transform: capitalize;
+  }
+`;
+
+export default Error;
