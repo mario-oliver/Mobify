ea921a08d51f8b747706c1e3c6bd2d3f5bb5cfc6
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-20 15:47:45 -0400

    beginning work on the login and register page. Form and styling inputted with default button/submit functions implememted

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import styled from 'styled-components';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  //global state and useNavigate
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo></Logo>
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className="form-row">
+          <label className="form-label">name</label>
+          <input
+            className="form-input"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            type="text"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  /* .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  } */
+`;
 
 export default Register;
