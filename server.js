898ef05dd77005db5d4191c7a68dd83b6962fcd4
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-22 11:44:06 -0400

    added the ES6 syntax to make node work with ES6 rather than CommonJS

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+/**
+ * to utilize ES6 instead of CommonJS for node.js:
+ *        - include "type" : "module" in package.json
+ * CommonJS syntax is the standard original language
+ *      - const express = require('express)
+ * But because front end is using ES6, it's worth utilizing ES6 in node js
+ */
+import express from 'express';
+const app = express();
+
 console.log('Server running...');
