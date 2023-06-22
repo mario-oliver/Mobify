af30eef344923401fbafc88e91f8e2e289671eb4
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-22 12:06:41 -0400

    added middleware for error handling and not found 404

--- server.js
+++ server.js
@@ -6,13 +6,20 @@
  * But because front end is using ES6, it's worth utilizing ES6 in node js
  */
 import express from 'express';
+import notFoundMiddleware from './middleware/note-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 const app = express();
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('Welcome');
 });
 
+//middleware
+app.use(notFoundMiddleware).use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  `Server is listening on port ${port}`;
+  console.log(`Server is listening on port ${port}...`);
 });
