054cfd9adfe815d37d0ccc33e03bd376d73c7f41
Mario Oliver <mariooliver@Marios-MBP.fios-router.home>
2023-06-23 12:46:05 -0400

    added user model to the database via mongoose.schema. Schema now provides certain requirements at the DB level for how a User record should look. Install validator from npm to provide battle tester email field validation through validator.isEmail function (envoked by the DB, reference passed)

---
+++ models/Users.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    trim: true,
+    minLength: 3,
+    maxLength: 20,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+    validate: {
+      validator: validator.isEmail(),
+      message: 'Please provide a valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minLength: 6,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    minLength: 3,
+    maxLength: 20,
+    default: 'LastName',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxLength: 20,
+    default: 'DC',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRoutes);
 
 //middleware
 app.use(notFoundMiddleware).use(errorHandlerMiddleware);
