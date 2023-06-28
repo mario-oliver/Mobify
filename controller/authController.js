8edec70af07dd3f463281f44d17e748a4bf5e771
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-28 10:55:08 -0400

    using npm package http-status-codes

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,10 @@
 import User from '../models/Users.js';
+import StatusCodes from 'http-status-codes';
 
 const register = async (req, res, next) => {
   //having access to req.body/json is from our app.use(express.json()) middleware
   const user = await User.create(req.body);
-  res.status(201).json({ user });
+  res.status(StatusCodes.CREATED).json({ user });
   /**
    * removed try catch
    * import 'express-async-errors' allows us to do this behind the scene with their package piping
