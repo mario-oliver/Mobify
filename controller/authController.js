54a0db65ffbede9ca183fa3d2efd0edf994f25fb
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-28 12:24:32 -0400

    refactored to move error classes to their own folder

--- controller/authController.js
+++ controller/authController.js
@@ -1,31 +1,12 @@
 import User from '../models/Users.js';
 import StatusCodes from 'http-status-codes';
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res, next) => {
   //having access to req.body/json is from our app.use(express.json()) middleware
   const { email, name, password } = req.body;
   if (!name || !email || !password)
-    throw new CustomAPIError('Please provide all the values');
+    throw new BadRequestError('Please provide all the values');
 
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
