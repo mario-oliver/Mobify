ed940c5731766bf124c8a56ab41f009eddc2f81a
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-28 12:14:45 -0400

    shifted error handling to be more robust. accepting an error based on what message and statuscode the new Error that's being thrown has. The error handler also now handles dynamic msg and errorcodes

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,20 @@
 import User from '../models/Users.js';
 import StatusCodes from 'http-status-codes';
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
 const register = async (req, res, next) => {
   //having access to req.body/json is from our app.use(express.json()) middleware
-  const user = await User.create(req.body);
+  const { email, name, password } = req.body;
+  if (!name || !email || !password)
+    throw new CustomAPIError('Please provide all the values');
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
   /**
    * removed try catch
