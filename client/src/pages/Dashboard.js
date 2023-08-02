d05a5a37b22b3ce6be5e5df943cf130d8f758131
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-02 10:57:29 -0400

    in connecting frontend and api, we have an issue. localhost:5000 cannot communicate with localhost:3000 -> two different servers/apps. this is due to CORS

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <div>Dashboard</div>;
 };
 
